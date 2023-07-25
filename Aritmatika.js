9937ddb9e66ff4abb71b1545f4c78d3746c8beb9
natasaaa14 <140400683+natasaaa14@users.noreply.github.com>
2023-07-25 15:08:40 +0700

    Aritmatika.js

---
+++ Aritmatika.js
@@ -0,0 +1,30 @@
+let bil1 = 8;
+         let bil2 = 7;
+        
+         let pertambahan=bil1+bil2;
+         let pengurangan=bil1-bil2;    
+         let perkalian  =bil1*bil2;
+         let pembagian  =bil1/bil2;
+         let modulus   =bil1%bil2;
+         
+document.write ("Hasil pertambahan"+
+     pertambahan+"<br>");
+document.write ("Hasil pengurangan"+ pengurangan+ "<br");
+document.write  ("Hasil perkalian"+ perkalian+ "<br>");
+document.write ("Hasil pembagian"+ pembagian+ "<br>");
+document.write ("Hasil modulus"+ modulus + "<kbr");      
+let bil1 = 8;
+         let bil2 = 7;
+        
+         let pertambahan=bil1+bil2;
+         let pengurangan=bil1-bil2;    
+         let perkalian  =bil1*bil2;
+         let pembagian  =bil1/bil2;
+         let modulus   =bil1%bil2;
+         
+document.write ("Hasil pertambahan"+
+     pertambahan+"<br>");
+document.write ("Hasil pengurangan"+ pengurangan+ "<br");
+document.write  ("Hasil perkalian"+ perkalian+ "<br>");
+document.write ("Hasil pembagian"+ pembagian+ "<br>");
+document.write ("Hasil modulus"+ modulus + "<kbr");      
